620a347ee6c286d97d45b89beb45f29862355545
manuelcampagnolo <mlc@isa.ulisboa.pt>
2015-06-11 14:36:03 +0100

    Update and rename read_write_sp_objects_to_shapefiles.r to read_write_sp_objects_to_shapefiles_and_kml.r

--- read_write_sp_objects_to_shapefiles_and_kml.r
+++ read_write_sp_objects_to_shapefiles_and_kml.r
@@ -6,6 +6,11 @@ plot(icnf)
 # exportar como shapefile
 writeOGR(icnf,dsn=getwd(),layer="ICNF",driver="ESRI Shapefile",overwrite_layer=TRUE)
 
+# exportar como kml (Google Earth)
+wgs84<-"+proj=longlat +ellps=WGS84 +datum=WGS84"
+icnf.wgs<-spTransform(icnf,CRS(wgs84))
+writeOGR(icnf.wgs,dsn="icnf.kml",layer="areas_protegidas",driver="KML",overwrite_layer=TRUE)
+
 # exportar raster para GeoTIFF
 writeRaster(mde.etrs,file="mde_sintra_cascais.tif",overwrite=TRUE)
 
